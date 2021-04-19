df66484ea1079a7061705c812c13921c0989d841
Nedjad <78599846+Nedjad@users.noreply.github.com>
2021-04-19 20:04:28 +0300

    Добавьте файлы проекта.

---
+++ Natural Number/Natural Number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Natural_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Введите число:");
+            number = int.Parse(Console.ReadLine());
+            NaturalNumb(number);
+
+        }
+
+        public static int NaturalNumb(int number)
+        {
+            int d = 0;
+            int i = 2;
+
+            while(i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++; 
+                           
+                }    
+                    
+                else
+                    i++;
+
+                i++;   
+            }
+
+            if (d == 0)
+                Console.WriteLine("Число простое.");
+            else
+                Console.WriteLine("Число не простое.");
+
+            return number;
+        }
+    }
+}
