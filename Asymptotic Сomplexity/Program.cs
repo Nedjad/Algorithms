e0f0e4858a823f6e9f0f3f1f8f2883650721e5bc
Nedjad <78599846+Nedjad@users.noreply.github.com>
2021-04-20 22:19:49 +0300

    Homework. Lesson 1

---
+++ Asymptotic Сomplexity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Asymptotic_Сomplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)  
+                    {
+                        int y = 0;
+
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+                        sum += inputArray[i] + i + k + j + y;
+                    }
+                }
+            }
+            return sum;
+        }
+        /*
+         * Асимптотическая сложность функции: O(N * N * N) = O(N^3)
+         */
+
+    }
+}
---
+++ Fibonacci Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите конец диапазона от 1 до : ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", Fib(i));
+            }
+            Console.ReadKey();
+
+            FibonacciCycle();
+        }
+
+        // Решение с рекурсией
+        static int Fib(int n)
+        {
+            if (n <= 2)
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+
+        // Решение с циклом
+        public static int FibonacciCycle()
+        {
+            Console.Write("Введите конец диапазона от 1 до : ");
+            int end = int.Parse(Console.ReadLine());
+            int j = 1;
+            for (int i = 1; i <= end; i += j)
+            {
+                Console.Write("{0} ", i);
+                j = i - j;
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
