7e3a707dadbc27e6fd3e9ca07d66fd5cc8953171
Nedjad <78599846+Nedjad@users.noreply.github.com>
2021-04-23 22:14:15 +0300

    Задание 2.2

---
+++ Binary Search/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 2, 6, 3, 5 };
+            Console.WriteLine(BinarySearch(array, 2)); 
+
+
+        }
+
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int minValue = 0;
+            int maxValue = inputArray.Length - 1;
+            while (minValue <= maxValue)
+            {
+                int mid = (minValue + maxValue) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    maxValue = mid - 1;
+                }
+                else
+                {
+                    minValue = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
