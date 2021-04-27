c511db0bd37c85cd5e70187a7a51f85d19fab5c0
Nedjad <78599846+Nedjad@users.noreply.github.com>
2021-04-27 20:56:33 +0300

    Занятие 4. Задание 2

---
+++ Lesson 4/Lesson 4.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myNewTree = new Node();
+            myNewTree.AddItem(30);
+            myNewTree.AddItem(25);
+            myNewTree.AddItem(41);
+            myNewTree.AddItem(20);
+            myNewTree.AddItem(27);
+            myNewTree.AddItem(50);
+            myNewTree.AddItem(35);
+            myNewTree.AddItem(61);
+            myNewTree.AddItem(40);
+            myNewTree.AddItem(28);
+            myNewTree.AddItem(15);
+            myNewTree.AddItem(34);
+            myNewTree.AddItem(26);
+
+            myNewTree.PrintMethodThree(myNewTree);
+
+
+
+
+
+        }
+    }
+}
