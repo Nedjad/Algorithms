59aa7cc0980967afeaa9aea67277036fe809c122
Nedjad <nedjad.mesh@gmail.com>
2021-05-12 23:52:35 +0300

    Задача 7

--- Lesson 7/Lesson 7/Program.cs
+++ Lesson 7/Lesson 7/Program.cs
@@ -1,43 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Lesson_7
 {
     class Program
     {
 
-        const int N = 3;
-        const int M = 3;
-
-        static void Step(int n, int m, int[,] a)
+        static void Main(string[] args)
         {
-            int i, j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < m; j++)
-                    Console.Write(a[i, j]);
-                Console.Write("\r\n");
-            }
+            int[,] matrix = new int[15, 15];
+            SearchWay sw = new SearchWay(15,15);
+            sw.PrintBoard(matrix);
+            
+
         }
 
-        static void Main()
-        {
-
-            int[,] A = new int[N, M];
-            int i, j;
-            for (j = 0; j < M; j++)
-                A[0, j] = 1; 
-            for (i = 1; i < N; i++)
-            {
-                A[i, 0] = 1;
-                for (j = 1; j < M; j++)
-                    A[i, j] = A[i, j - 1] + A[i - 1, j];
-            }
-
-            Step(N, M, A);
-
-
-        }
         
-
     }
 }
+
+
+       
+           
---
+++ Lesson 7/Lesson 7/SearchWay.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7
+{
+    class SearchWay
+    {
+
+        public int N { get; set; } //Количество строк
+        public int M { get; set; } //Количество столбцов
+
+        public SearchWay(int row, int column)
+        {
+            N = row;
+            M = column;
+        }
+
+        /// <summary>
+        /// Метод вывода доски (матрицы) на экран
+        /// </summary>
+        public void PrintBoard(int[,] map)
+        {
+            Console.WriteLine("Количество маршрутов");
+            Console.WriteLine();
+            for (int i = 0; i < M; i++)
+            {
+                Console.Write("---+");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    var result = map[i, j];
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("|");
+                    if (map[i, j] == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write("{0, 3}", result);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("|");
+                Console.Write("\r\n");
+            }
+            for (int i = 0; i < M; i++)
+            {
+                Console.Write("+---");
+            }
+        }
+
+        
+        
+    }
+}
